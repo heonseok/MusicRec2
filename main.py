2e73b6388ee7d0288da1fee5499feee091617624
heonseok <heonseok.ha@gmail.com>
2017-08-11 12:28:27 +0900

    refacotring AE model from main code

--- main.py
+++ main.py
@@ -2,55 +2,134 @@ import tensorflow as tf
 import numpy as np
 import os
 import logging
+from ae import AE
 
-class AE():
-    def __init__(self, logger, gpu_id, learning_rate, input_dim, ae_h_dim_list, z_dim):
-        self.logger = logger
-        self.gpu_id = gpu_id
 
-        self.learning_rate = learning_rate 
+def main(_):
+    logger.info("Running AE")
+    model = AE(logger, FLAGS.gpu_id, FLAGS.learning_rate, input_dim, eval(FLAGS.ae_h_dim_list), FLAGS.z_dim)
 
-        self.input_dim = input_dim
-        self.z_dim = z_dim
+    init = tf.global_variables_initializer()
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    #config.log_device_placement = True
 
-        self.enc_h_dim_list = [*ae_h_dim_list, z_dim]
-        self.dec_h_dim_list = [*list(reversed(ae_h_dim_list))]
+    #merged = tf.summary.merge_all()
 
-        self.build_model()
+    with tf.device('cpu:0'):
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.max_to_keep)
 
-    def build_model(self):
-        with tf.device('/gpu:%d' % self.gpu_id):
-            self.X = tf.placeholder(tf.float32, [None, self.input_dim])
+    with tf.Session(config=config) as sess:
+        sess.run(init)
 
-            previous_layer = self.X
-            for idx, enc_h_dim in enumerate(self.enc_h_dim_list):
-                #print(idx, enc_h_dim)
-                previous_layer = tf.layers.dense(inputs=previous_layer, units=enc_h_dim, activation=tf.nn.relu)
+        #writer = tf.train.SummaryWriter(tensorboard_dir, sess.graph)
+        train_batch_total = int(data.train.num_examples / FLAGS.batch_size)
+        valid_batch_total = int(data.validation.num_examples/ FLAGS.batch_size)
 
-            for idx, dec_h_dim in enumerate(self.dec_h_dim_list):
-                #print(idx, dec_h_dim)
-                previous_layer = tf.layers.dense(inputs=previous_layer, units=dec_h_dim, activation=tf.nn.relu)
+        train_logging_step = int(train_batch_total/10);
+        valid_logging_step = int(valid_batch_total/10);
 
-            output = tf.layers.dense(inputs=previous_layer, units=self.input_dim, activation=tf.nn.tanh) #, kernel_initializer=tf.contrib.layers.xavier_initializer)
-            #cost = tf.reduce_mean(tf.square(X-output))
-            self.cost = tf.losses.mean_squared_error(self.X, output)
-            #cost_summary = tf.summary.scalar('cost', cost)
-            self.solver = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.cost)
+        best_valid_total_cost = float('inf')
+        best_model_idx = 0
+        #best_save_path = ''
+        valid_non_improve_count = 0
 
-    def train(self, sess, batch_xs, epoch_idx, batch_idx, train_batch_total, log_flag):
-        _, cost_val = sess.run([self.solver, self.cost], feed_dict={self.X: batch_xs})
-        if log_flag == True:
-            self.logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
-        #ogger.debug('Epoch %.3i, Train loss: %.4E' % (epoch_idx+1, train_total_cost / train_batch_total))
-        return cost_val
-        
+        for epoch_idx in range(FLAGS.epoch):
+            train_total_cost = 0
+            valid_total_cost = 0
+            
+            ##### TRAIN #####
+            for batch_idx in range(train_batch_total):
+                if mnist_flag == True:
+                    batch_xs, batch_ys = data.train.next_batch(FLAGS.batch_size)
+                elif mnist_flag == False:
+                    batch_xs = data.train.next_batch(FLAGS.batch_size)
 
-    def inference(self, sess, batch_xs, epoch_idx, batch_idx, train_batch_total, log_flag):
-        cost_val = sess.run(self.cost, feed_dict={self.X: batch_xs})
-        if log_flag == True:
-            self.logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
-        return cost_val
+                if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
+                    #logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
+                    log_flag = True
+                else:
+                    log_flag = False
 
+                #_, cost_val, summary = sess.run([solver, cost, merged], feed_dict={X: batch_xs})
+                #writer.add_summary(summary, global_step=epoch_idx*tranin_total_batch+batch_idx)
+                cost_val = model.train(sess, batch_xs, epoch_idx, batch_idx, train_batch_total, log_flag)
+                #_, cost_val = sess.run([solver, cost], feed_dict={X: batch_xs})
+                train_total_cost += cost_val
+
+                #if((batch_idx+1) % train_logging_step == 0):
+            logger.debug('Epoch %.3i, Train loss: %.4E' % (epoch_idx+1, train_total_cost / train_batch_total))
+            save_path = saver.save(sess, ckpt_path, global_step=epoch_idx) 
+
+
+            ##### VALIDATION #####
+            for batch_idx in range(valid_batch_total):
+                if mnist_flag == True:
+                    batch_xs, batch_ys = data.validation.next_batch(FLAGS.batch_size)
+                elif mnist_flag == False:
+                    batch_xs = data.validation.next_batch(FLAGS.batch_size)
+
+                if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
+                    #logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
+                    log_flag = True
+                else:
+                    log_flag = False
+
+                cost_val = model.inference(sess, batch_xs, epoch_idx, batch_idx, train_batch_total, log_flag)
+                #cost_val = sess.run(cost, feed_dict={X: batch_xs})
+                valid_total_cost += cost_val
+
+                #if ((batch_idx+1) % valid_logging_step == 0):
+                #if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
+                    #logger.debug('Epoch %.3i, Batch[%.3i/%i], Valid loss: %.4E' % (epoch_idx + 1, batch_idx + 1, valid_batch_total, cost_val))
+            logger.debug('Epoch %.3i, Valid loss: %.4E' % (epoch_idx + 1, valid_total_cost / valid_batch_total))
+
+            ## Update best_valid_total_cost
+            if valid_total_cost < best_valid_total_cost:
+                best_valid_total_cost = valid_total_cost
+                valid_non_improve_count = 0
+                best_model_idx = epoch_idx 
+                #best_save_path = save_path
+            else:
+                valid_non_improve_count += 1
+                logger.info("Valid cost has not been improved for %d epochs" % valid_non_improve_count)
+                if valid_non_improve_count == 10:
+                    break
+
+            if mnist_flag == True:
+                sample_size = 16
+                samples = sess.run(output, feed_dict={X: data.test.images[:sample_size]})
+                fig = drawer.plot(samples)
+                plt.savefig(image_dir + '/{}.png'.format(str(epoch_idx+1).zfill(3)), bbox_inches='tight')
+
+        logger.info("Best model idx : " + str(best_model_idx))
+
+    ##### TEST #####
+    if FLAGS.dataset == "Music":
+        with tf.Session(config=config) as sess:
+            test_total_cost = 0
+            saver.restore(sess, ckpt_path+"-"+str(best_model_idx))
+
+            test_batch_total = int(data.test.num_examples / FLAGS.batch_size)
+            test_logging_step = int(test_batch_total/10);
+
+            for batch_idx in range(test_batch_total):
+                batch_xs, batch_idxs = data.test.next_batch_with_idx(FLAGS.batch_size)
+
+                if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
+                    #logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
+                    log_flag = True
+                else:
+                    log_flag = False
+
+                cost_val = model.inference(sess, batch_xs, epoch_idx, batch_idx, train_batch_total, log_flag)
+                #cost_val = sess.run(cost, feed_dict={X: batch_xs})
+                test_total_cost += cost_val
+                #if ((batch_idx+1) % test_logging_step == 0):
+                #if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
+                    #logger.debug('Epoch %.3i, Batch[%.3i/%i], Test loss: %.4E' % (epoch_idx + 1, batch_idx + 1, test_batch_total, cost_val))
+            logger.debug('Epoch %.3i, Test loss: %.4E' % (epoch_idx + 1, test_total_cost / test_batch_total))
+           
 """
 def main(_):
     logger.info("Running AE")
@@ -176,9 +255,7 @@ def main(_):
                     if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
                         logger.debug('Epoch %.3i, Batch[%.3i/%i], Test loss: %.4E' % (epoch_idx + 1, batch_idx + 1, test_batch_total, cost_val))
                 logger.debug('Epoch %.3i, Test loss: %.4E' % (epoch_idx + 1, test_total_cost / test_batch_total))
-           
-
-
+"""
 if __name__ == '__main__':
     flags = tf.app.flags
 
@@ -199,7 +276,7 @@ if __name__ == '__main__':
     flags.DEFINE_integer("batch_logging_step", 10, "Batch size [100]")
 
 
-    flags.DEFINE_integer("max_to_keep", "10", "maximum number of recent checkpoint files to keep[ckpt]")
+    flags.DEFINE_integer("max_to_keep", "11", "maximum number of recent checkpoint files to keep[ckpt]")
 
     #flags.DEFINE_string("data_dir", "data", 'Directory name to load input data [data]')
     #flags.DEFINE_string("checkpoint_dir", "ckpt", "Directory name to save the checkpoints [checkpoint]")
@@ -257,4 +334,3 @@ if __name__ == '__main__':
     logger.addHandler(fh)
 
     tf.app.run()
-"""
