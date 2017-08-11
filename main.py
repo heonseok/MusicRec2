b6c33f3106c2c0602b506da1d972038550bc5939
heonseok <heonseok.ha@gmail.com>
2017-08-11 14:20:31 +0900

    add if condition for train and write best_model_idx.txt for testing

--- main.py
+++ main.py
@@ -19,96 +19,106 @@ def main(_):
     with tf.device('cpu:0'):
         saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.max_to_keep)
 
-    with tf.Session(config=config) as sess:
-        sess.run(init)
+    if FLAGS.is_train == True:
+        with tf.Session(config=config) as sess:
+            sess.run(init)
 
-        #writer = tf.train.SummaryWriter(tensorboard_dir, sess.graph)
-        train_batch_total = int(data.train.num_examples / FLAGS.batch_size)
-        valid_batch_total = int(data.validation.num_examples/ FLAGS.batch_size)
+            #writer = tf.train.SummaryWriter(tensorboard_dir, sess.graph)
+            train_batch_total = int(data.train.num_examples / FLAGS.batch_size)
+            valid_batch_total = int(data.validation.num_examples/ FLAGS.batch_size)
 
-        train_logging_step = int(train_batch_total/10);
-        valid_logging_step = int(valid_batch_total/10);
+            train_logging_step = int(train_batch_total/10);
+            valid_logging_step = int(valid_batch_total/10);
 
-        best_valid_total_cost = float('inf')
-        best_model_idx = 0
-        #best_save_path = ''
-        valid_non_improve_count = 0
+            best_valid_total_cost = float('inf')
+            best_model_idx = 0
+            #best_save_path = ''
+            valid_non_improve_count = 0
 
-        for epoch_idx in range(FLAGS.epoch):
-            train_total_cost = 0
-            valid_total_cost = 0
-            
-            ##### TRAIN #####
-            for batch_idx in range(train_batch_total):
-                if mnist_flag == True:
-                    batch_xs, batch_ys = data.train.next_batch(FLAGS.batch_size)
-                elif mnist_flag == False:
-                    batch_xs = data.train.next_batch(FLAGS.batch_size)
+            for epoch_idx in range(FLAGS.epoch):
+                train_total_cost = 0
+                valid_total_cost = 0
+                
+                ##### TRAIN #####
+                for batch_idx in range(train_batch_total):
+                    if mnist_flag == True:
+                        batch_xs, batch_ys = data.train.next_batch(FLAGS.batch_size)
+                    elif mnist_flag == False:
+                        batch_xs = data.train.next_batch(FLAGS.batch_size)
 
-                if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
-                    #logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
-                    log_flag = True
+                    if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
+                        #logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
+                        log_flag = True
+                    else:
+                        log_flag = False
+
+                    #_, cost_val, summary = sess.run([solver, cost, merged], feed_dict={X: batch_xs})
+                    #writer.add_summary(summary, global_step=epoch_idx*tranin_total_batch+batch_idx)
+                    cost_val = model.train(sess, batch_xs, epoch_idx, batch_idx, train_batch_total, log_flag)
+                    #_, cost_val = sess.run([solver, cost], feed_dict={X: batch_xs})
+                    train_total_cost += cost_val
+
+                    #if((batch_idx+1) % train_logging_step == 0):
+                logger.debug('Epoch %.3i, Train loss: %.4E' % (epoch_idx+1, train_total_cost / train_batch_total))
+                save_path = saver.save(sess, ckpt_path, global_step=epoch_idx) 
+
+
+                ##### VALIDATION #####
+                for batch_idx in range(valid_batch_total):
+                    if mnist_flag == True:
+                        batch_xs, batch_ys = data.validation.next_batch(FLAGS.batch_size)
+                    elif mnist_flag == False:
+                        batch_xs = data.validation.next_batch(FLAGS.batch_size)
+
+                    if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
+                        #logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
+                        log_flag = True
+                    else:
+                        log_flag = False
+
+                    cost_val = model.inference(sess, batch_xs, epoch_idx, batch_idx, valid_batch_total, log_flag)
+                    #cost_val = sess.run(cost, feed_dict={X: batch_xs})
+                    valid_total_cost += cost_val
+
+                    #if ((batch_idx+1) % valid_logging_step == 0):
+                    #if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
+                        #logger.debug('Epoch %.3i, Batch[%.3i/%i], Valid loss: %.4E' % (epoch_idx + 1, batch_idx + 1, valid_batch_total, cost_val))
+                logger.debug('Epoch %.3i, Valid loss: %.4E' % (epoch_idx + 1, valid_total_cost / valid_batch_total))
+
+                ## Update best_valid_total_cost
+                if valid_total_cost < best_valid_total_cost:
+                    best_valid_total_cost = valid_total_cost
+                    valid_non_improve_count = 0
+                    best_model_idx = epoch_idx 
+                    #best_save_path = save_path
                 else:
-                    log_flag = False
+                    valid_non_improve_count += 1
+                    logger.info("Valid cost has not been improved for %d epochs" % valid_non_improve_count)
+                    if valid_non_improve_count == 10:
+                        break
 
-                #_, cost_val, summary = sess.run([solver, cost, merged], feed_dict={X: batch_xs})
-                #writer.add_summary(summary, global_step=epoch_idx*tranin_total_batch+batch_idx)
-                cost_val = model.train(sess, batch_xs, epoch_idx, batch_idx, train_batch_total, log_flag)
-                #_, cost_val = sess.run([solver, cost], feed_dict={X: batch_xs})
-                train_total_cost += cost_val
-
-                #if((batch_idx+1) % train_logging_step == 0):
-            logger.debug('Epoch %.3i, Train loss: %.4E' % (epoch_idx+1, train_total_cost / train_batch_total))
-            save_path = saver.save(sess, ckpt_path, global_step=epoch_idx) 
-
-
-            ##### VALIDATION #####
-            for batch_idx in range(valid_batch_total):
                 if mnist_flag == True:
-                    batch_xs, batch_ys = data.validation.next_batch(FLAGS.batch_size)
-                elif mnist_flag == False:
-                    batch_xs = data.validation.next_batch(FLAGS.batch_size)
+                    sample_size = 16
+                    samples = sess.run(output, feed_dict={X: data.test.images[:sample_size]})
+                    fig = drawer.plot(samples)
+                    plt.savefig(image_dir + '/{}.png'.format(str(epoch_idx+1).zfill(3)), bbox_inches='tight')
 
-                if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
-                    #logger.debug('Epoch %.3i, Batch[%.3i/%i], Train loss: %.4E' % (epoch_idx + 1, batch_idx + 1, train_batch_total, cost_val))
-                    log_flag = True
-                else:
-                    log_flag = False
-
-                cost_val = model.inference(sess, batch_xs, epoch_idx, batch_idx, valid_batch_total, log_flag)
-                #cost_val = sess.run(cost, feed_dict={X: batch_xs})
-                valid_total_cost += cost_val
-
-                #if ((batch_idx+1) % valid_logging_step == 0):
-                #if ((batch_idx+1) % FLAGS.batch_logging_step == 0):
-                    #logger.debug('Epoch %.3i, Batch[%.3i/%i], Valid loss: %.4E' % (epoch_idx + 1, batch_idx + 1, valid_batch_total, cost_val))
-            logger.debug('Epoch %.3i, Valid loss: %.4E' % (epoch_idx + 1, valid_total_cost / valid_batch_total))
-
-            ## Update best_valid_total_cost
-            if valid_total_cost < best_valid_total_cost:
-                best_valid_total_cost = valid_total_cost
-                valid_non_improve_count = 0
-                best_model_idx = epoch_idx 
-                #best_save_path = save_path
-            else:
-                valid_non_improve_count += 1
-                logger.info("Valid cost has not been improved for %d epochs" % valid_non_improve_count)
-                if valid_non_improve_count == 10:
-                    break
-
-            if mnist_flag == True:
-                sample_size = 16
-                samples = sess.run(output, feed_dict={X: data.test.images[:sample_size]})
-                fig = drawer.plot(samples)
-                plt.savefig(image_dir + '/{}.png'.format(str(epoch_idx+1).zfill(3)), bbox_inches='tight')
-
-        logger.info("Best model idx : " + str(best_model_idx))
+            logger.info("Best model idx : " + str(best_model_idx))
+            f = open(ckpt_path+'/best_model_idx.txt','w')
+            f.write(best_model)
+            f.close()
 
     ##### TEST #####
     if FLAGS.dataset == "Music":
         with tf.Session(config=config) as sess:
             test_total_cost = 0
+            if FLAGS.is_train == False:
+                if tf.train.get_checkpoint_state(ckpt_dir):
+                    f = open(ckpt_dir+'/best_model_idx.txt','r')
+                    best_model_idx = f.readline().strip()
+
             saver.restore(sess, ckpt_path+"-"+str(best_model_idx))
+            epoch_idx = eval(best_model_idx)
 
             test_batch_total = int(data.test.num_examples / FLAGS.batch_size)
             test_logging_step = int(test_batch_total/10);
